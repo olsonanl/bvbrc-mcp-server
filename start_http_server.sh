9b6ec47c97b4eccca1f2e8c08efdd9bdc1cc2ae4
Robert Olson <olson@mcs.anl.gov>
2025-11-11 12:57:04 -0600

    Daemonize server startup

--- start_http_server.sh
+++ start_http_server.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+cd $DIR
 source mcp_env/bin/activate
-python3 http_server.py
+#python3 http_server.py
+daemonize \
+	-a \
+	-e $DIR/server.stderr \
+	-o $DIR/server.stdout \
+	-c $DIR \
+	-v \
+	-p $DIR/server.pid \
+	$DIR/mcp_env/bin/python3 $DIR/http_server.py
